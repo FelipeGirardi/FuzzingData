cb01bf23225c83d3e69eb3b451de08e9380ee8ef
Felipe Girardi <felipegirardifg@gmail.com>
2022-10-13 14:13:01 +0200

    Adjustments

--- plot_data/mean_graph.py
+++ plot_data/mean_graph.py
@@ -8,9 +8,9 @@ for i in range(1,9):
     df = pd.read_csv('fuzzer0' + str(i), sep='\,\ ', engine='python')
     df.columns = df.columns.str.replace('# ', '')
 
-    df = df.groupby('edges_found').mean()
-    relative_time_list = df['relative_time'].tolist()
-    edges_found_list = df.index.values
+    df = df.groupby('relative_time').mean()
+    relative_time_list = df.index.values
+    edges_found_list = df['edges_found'].tolist()
 
     plt.figure(1)
     xmin, xmax = min(relative_time_list), max(relative_time_list)
@@ -22,17 +22,14 @@ for i in range(1,9):
     plt.plot(relative_time_list, edges_found_list, linewidth = 2.0, color=colors[i-1], label='fuzzer0' + str(i))
     leg = plt.legend(loc='lower right')
     
+    # total_execs plot
+    total_execs_list = df['total_execs'].tolist()
+    plt.figure(2)
+    ymin, ymax = min(total_execs_list), max(total_execs_list)
+    plt.ylim(ymin, ymax)
+    plt.xlim(xmin, xmax)
+    plt.plot(relative_time_list, total_execs_list, linewidth = 2.0, color=colors[i-1])
+    plt.xlabel('Time (s)')
+    plt.ylabel('Total executions')
+
 plt.show()
-
-
-# total_execs plot
-
-# total_execs_list = df['total_execs'].tolist()
-# plt.figure(2)
-# ymin, ymax = min(total_execs_list), max(total_execs_list)
-# plt.ylim(ymin, ymax)
-# plt.xlim(xmin, xmax)
-# plt.plot(relative_time_list, total_execs_list, linewidth = 2.0, color='red')
-# plt.xlabel('Time (s)')
-# plt.ylabel('Total executions')
-# plt.show()
--- plot_data/median_graph.py
+++ plot_data/median_graph.py
@@ -2,32 +2,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('fuzzer01', sep='\,\ ', engine='python')
-df.columns = df.columns.str.replace('# ', '')
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', '#808080']
 
-time_group = df.groupby('relative_time')
-mean_edges = time_group.mean().rename(columns={"edges_found": "mean_edges_found"})
-edges_25per = time_group.quantile(q=.25).rename(columns={"edges_found": "edges_found_25per"})
-edges_75per = time_group.quantile(q=.75).rename(columns={"edges_found": "edges_found_75per"})
+for i in range(1,9):
+    df = pd.read_csv('fuzzer0' + str(i), sep='\,\ ', engine='python')
+    df.columns = df.columns.str.replace('# ', '')
 
-dframes = [mean_edges, edges_25per, edges_75per]
-merged_df = pd.concat(dframes, join='outer', axis=1)
+    time_group = df.groupby('relative_time')
+    mean_edges = time_group.mean().rename(columns={"edges_found": "mean_edges_found"})
+    edges_25per = time_group.quantile(q=.25).rename(columns={"edges_found": "edges_found_25per"})
+    edges_75per = time_group.quantile(q=.75).rename(columns={"edges_found": "edges_found_75per"})
 
-x = merged_df.index
-y = merged_df.mean_edges_found
-y_low = merged_df.edges_found_25per
-y_high = merged_df.edges_found_75per
+    dframes = [mean_edges, edges_25per, edges_75per]
+    merged_df = pd.concat(dframes, join='outer', axis=1)
 
-plt.plot(x, y, color='darkorchid', label='mean') 
-plt.plot(x, y_low, color='darkorchid', linestyle='-.', label='25th %') 
-plt.plot(x, y_high, color='darkorchid', linestyle='-.', label='75th %') 
-plt.legend()
+    x = merged_df.index
+    y = merged_df.mean_edges_found
+    y_low = merged_df.edges_found_25per
+    y_high = merged_df.edges_found_75per
 
-plt.fill_between(x, y_low, y_high, alpha =.1, color = 'darkorchid')
+    plt.figure(1)
+    plt.plot(x, y, color=colors[i-1]) 
+    plt.plot(x, y_low, color=colors[i-1], linestyle='-.') 
+    plt.plot(x, y_high, color=colors[i-1], linestyle='-.')
 
-# plt.grid(alpha=.2, which='both')
-plt.xlabel('Time (s)')
-plt.ylabel('Edges found')
+    plt.fill_between(x, y_low, y_high, alpha =.1, color = colors[i-1])
+
+    plt.xlabel('Time (s)')
+    plt.ylabel('Edges found')
+    
 plt.show()
 
 # df_stats = df.groupby(['relative_time']).describe()
