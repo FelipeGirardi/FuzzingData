9ddaa5d35fdb424778f3a0fe020dc9b248e047ca
Felipe Girardi <felipegirardifg@gmail.com>
2023-01-25 13:31:53 +0100

    Attempt at changing subplot sizes to form treemap

--- treemaps/treemaps_fuzzing.py
+++ treemaps/treemaps_fuzzing.py
@@ -9,7 +9,7 @@ import squarify
 COVERAGE_DATA_FILE = sys.argv[1]
 
 def plotSubgraph(values, label, ax, fileName):
-    ax.set_title(label, fontsize='8')
+    # ax.set_title(label, fontsize='8')
     is_value_covered_0 = values[0] == 0
     is_value_not_covered_0 = values[1] == 0
     values = [values[1]] if is_value_covered_0 else [values[0]] if is_value_not_covered_0 else values
@@ -31,9 +31,7 @@ df = pd.read_csv(COVERAGE_DATA_FILE, engine='python')
 df.columns = df.columns.str.replace('# ', '')
 df = df[df['edges_total'] != 0]
 df['edges_not_covered'] = df['edges_total'] - df['edges_covered']
-df['percent_covered'] = df['edges_covered']/df['edges_total']
-df['percent_not_covered'] = df['edges_not_covered']/df['edges_total']
-df = df.sort_values('percent_not_covered', ascending=False)
+df = df.sort_values('edges_not_covered', ascending=False)
 
 edges_covered = df['edges_covered'].values
 edges_total = df['edges_total'].values
@@ -46,13 +44,20 @@ no_covered_edges_indexes = []
 # Treemap 1: edges covered in green, edges not covered in red
 n_rows_plot = round(math.sqrt(n_rows_df))
 n_columns_plot = math.ceil(math.sqrt(n_rows_df))
-fig, axs = plt.subplots(n_rows_plot, n_columns_plot)
-plt.subplots_adjust(hspace=0.4, wspace=0.4)
+n_plots = n_rows_plot * n_columns_plot
+height_ratios = []
+edges_not_covered_split = np.array_split(edges_not_covered, n_rows_plot)
+width_ratios = np.mean(edges_not_covered_split, axis=0)
+for arr in edges_not_covered_split:
+    height_ratios.append(np.mean(arr))
+
+fig, axs = plt.subplots(n_rows_plot, n_columns_plot, gridspec_kw={'width_ratios': width_ratios, 'height_ratios': height_ratios})
+plt.subplots_adjust(hspace=0, wspace=0)
 
 for ax1 in axs:
     for ax2 in ax1:
         ax2.axis('off')
-fig.suptitle('Edges covered and not covered for ' + file_name)
+fig.suptitle('Edges not covered for ' + file_name)
 
 for index, value_covered in enumerate(edges_covered):
     if value_covered == 0:
@@ -62,17 +67,17 @@ for index, value_covered in enumerate(edges_covered):
     plotSubgraph(values, labels[index], axs[index // n_columns_plot][index % n_columns_plot], 'edge_coverage_treemap')
 
 # Treemap 2: percentage of edges covered
-for i, _ in enumerate(no_covered_edges_indexes):
-    edges_covered_copy = np.delete(edges_covered, i)
-    edges_total_copy = np.delete(edges_total, i)
-    labels_copy = np.delete(labels, i)
-    
+for _, num in enumerate(no_covered_edges_indexes):
+    edges_covered_copy = np.delete(edges_covered, num)
+    edges_total_copy = np.delete(edges_total, num)
+    labels_copy = np.delete(labels, num)
+
 percent_edges_covered_not_rounded = (edges_covered_copy/edges_total_copy)*100
 percent_edges_covered_rounded = []
 for i, elem in enumerate(percent_edges_covered_not_rounded):
     n_rounded = np.around(elem, 1)
     percent_edges_covered_rounded.append(n_rounded)
-    n_rounded_str = '\n' + str(n_rounded) + ' %'
+    n_rounded_str = '\n' + str(n_rounded) + '%'
     labels_copy[i] += n_rounded_str
 plt.figure()
 plt.title('Percentage of edges covered for ' + file_name)
