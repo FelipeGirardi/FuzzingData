43b0e0be86a023951b80b90ee94bc933a3ad1ecf
Felipe Girardi <felipegirardifg@gmail.com>
2023-02-02 17:56:54 +0100

    Base differetial treemap done, small thing to fix

--- treemaps/treemaps_fuzzing.py
+++ treemaps/treemaps_fuzzing.py
@@ -7,7 +7,8 @@ import numpy as np
 import squarify
 import plotly.express as px
 
-COVERAGE_DATA_FILE = sys.argv[1]
+COVERAGE_DATA_FILE_1 = sys.argv[1]
+COVERAGE_DATA_FILE_2 = sys.argv[2]
 
 def plotSubgraph(values, label, ax):
     ax.set_title(label, fontsize='8')
@@ -26,59 +27,28 @@ def plotPercentageGraph(values, labels, fileName):
     squarify.plot(sizes=values, alpha=0.8, edgecolor="white", label=labels, color=colors, text_kwargs={'fontsize':8})
     plt.savefig(fileName)
 
-# Get edge data
-df = pd.read_csv(COVERAGE_DATA_FILE, engine='python')
+# Get edge data for main file
+df = pd.read_csv(COVERAGE_DATA_FILE_1, engine='python')
 df.columns = df.columns.str.replace('# ', '')
 df = df[df['edges_total'] != 0]
 df['edges_not_covered'] = df['edges_total'] - df['edges_covered']
-df = df.sort_values('edges_not_covered', ascending=False)
+df_sorted = df.sort_values('edges_not_covered', ascending=False)
 
-edges_covered = df['edges_covered'].values
-edges_total = df['edges_total'].values
-edges_not_covered = df['edges_not_covered'].values
-labels = df['function'].values
-file_name = df['file'].iloc[0]
-n_rows_df = len(df)
-no_covered_edges_indexes = []
-
-# Treemap 1: edges covered in green, edges not covered in red
-# n_rows_plot = round(math.sqrt(n_rows_df))
-# n_columns_plot = math.ceil(math.sqrt(n_rows_df))
-# n_plots = n_rows_plot * n_columns_plot
-# height_ratios = []
-# edges_not_covered_split = np.array_split(edges_not_covered, n_rows_plot)
-# width_ratios = np.mean(edges_not_covered_split, axis=0)
-# for arr in edges_not_covered_split:
-#     height_ratios.append(np.mean(arr))
-
-# fig, axs = plt.subplots(n_rows_plot, n_columns_plot, gridspec_kw={'width_ratios': width_ratios, 'height_ratios': height_ratios})
-# plt.subplots_adjust(hspace=0.5, wspace=0.1)
-
-# for ax1 in axs:
-#     for ax2 in ax1:
-#         ax2.axis('off')
-# fig.suptitle('Edges not covered for ' + file_name)
-
-for index, value_covered in enumerate(edges_covered):
-    if value_covered == 0:
-        no_covered_edges_indexes.append(index)
-    # value_not_covered = edges_not_covered[index]
-    # values = [value_covered, value_not_covered]
-    # plotSubgraph(values, labels[index], axs[index // n_columns_plot][index % n_columns_plot])
-
-# plt.savefig('edge_coverage_treemap')
+edges_covered = df_sorted['edges_covered'].values
+edges_total = df_sorted['edges_total'].values
+edges_not_covered = df_sorted['edges_not_covered'].values
+labels = df_sorted['function'].values
+file_name = df_sorted['file'].iloc[0]
 
 # Treemap 1: edges covered in green, edges not covered in red (plotly)
-df_covered = df.copy()
+df_covered = df_sorted.copy()
 df_covered = df_covered.drop('edges_not_covered', axis=1)
 df_covered = df_covered.rename({'edges_covered': 'edges'}, axis=1)
 df_not_covered = df_covered.copy()
 df_covered['covered'] = ['Covered'] * len(df_covered)
-df_covered['color'] = ['green'] * len(df_covered)
 
 df_not_covered['edges'] = edges_not_covered
 df_not_covered['covered'] = ['Not covered'] * len(df_not_covered)
-df_covered['color'] = ['red'] * len(df_not_covered)
 df_all = pd.concat([df_covered, df_not_covered])
 
 fig = px.treemap(df_all, path=['file', 'function', 'edges'], values='edges', color='covered', color_discrete_map={'(?)': 'lightgrey'})
@@ -87,6 +57,12 @@ fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
 fig.show()
 
 # Treemap 2: percentage of edges covered (squarify)
+no_covered_edges_indexes = []
+
+for index, value_covered in enumerate(edges_covered):
+    if value_covered == 0:
+        no_covered_edges_indexes.append(index)
+
 for _, num in enumerate(no_covered_edges_indexes):
     edges_covered_copy = np.delete(edges_covered, num)
     edges_total_copy = np.delete(edges_total, num)
@@ -104,9 +80,35 @@ plt.title('Percentage of edges covered for ' + file_name)
 plt.axis('off')
 plotPercentageGraph(percent_edges_covered_rounded, labels_copy, 'perc_edges_covered_treemap')
 
-# # Treemap: percentage of edges not covered
-# percent_edges_not_covered = 100-percent_edges_covered
-# percent_edges_not_covered = percent_edges_not_covered[percent_edges_not_covered != 0]
-# plotGraph(percent_edges_not_covered, labels, 'perc_edges_not_covered_treemap', 'Percentage of edges not covered for ' + file_name)
+# Treemap 3: difference in edges covered between 2 files
+df2 = pd.read_csv(COVERAGE_DATA_FILE_2, engine='python')
+df2.columns = df2.columns.str.replace('# ', '')
+df2 = df2[df2['edges_total'] != 0]
 
-plt.show()
+covered_edges_diffs = []
+covered_edges_remainders = []
+
+for i in df.index:
+    diff = abs(df['edges_covered'][i] - df2['edges_covered'][i])
+    remainder = df['edges_total'][i] - diff
+    covered_edges_diffs.append(diff)
+    covered_edges_remainders.append(remainder)
+    
+df_diffs = df2.copy()
+df_diffs['diffs'] = pd.Series(covered_edges_diffs)
+df_diffs['isDiffOrRemainder'] = ['Diff'] * len(df_diffs)
+df_remainders = df2.copy()
+df_remainders['diffs'] = pd.Series(covered_edges_remainders)
+df_remainders['isDiffOrRemainder'] = ['Remainder'] * len(df_remainders)
+df_plot = pd.concat([df_diffs, df_remainders], ignore_index=True)
+df_plot = df_plot.drop('edges_covered', axis=1)
+df_plot = df_plot.drop('edges_total', axis=1)
+
+print(df_plot)
+
+fig2 = px.treemap(df_plot, path=['file', 'function', 'diffs'], values='diffs', branchvalues="total", color='isDiffOrRemainder', color_discrete_map={'(?)': 'lightgrey', 'Diff': 'green', 'Remainder': 'gray'})
+fig2.update_traces(root_color="lightgrey")
+fig2.update_layout(margin = dict(t=50, l=25, r=25, b=25))
+fig2.show()
+
+# plt.show()
